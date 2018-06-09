769f07949233ab5c3fa212935376dd76a38305e3
JANGYOUNJEE <sg04041@naver.com>
2018-06-09 23:07:19 +0900

    Sina_Ver26

---
+++ Sina_ver26/app/src/main/java/org/androidtown/sina_ver01/DiaryActivity.java
@@ -0,0 +1,171 @@
+package org.androidtown.sina_ver01;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.util.Util;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+
+public class DiaryActivity extends AppCompatActivity implements View.OnClickListener {
+    DatabaseReference mRef;
+    DiaryData diaryData;
+    ImageButton saveBtn;
+    ImageButton pSaveBtn;
+
+    TextView lDate;
+    Button lWeather;
+    EditText lTitle;
+    TextView lSubtitle;
+    EditText lContent;
+
+    TextView pSubtitle;
+    TextView pDate;
+    Button pWeather;
+    ImageView pView;
+    EditText pContent;
+    ImageButton pUploadBtn;
+
+    TextView dSubtitle;
+    TextView dDate;
+    Button dWeather;
+    ImageView dView;
+    EditText dContents;
+    ImageButton dSaveBtn;
+    int emotion;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       //setContentView(R.layout.activity_diary);
+
+
+        Intent myIntent = getIntent();
+        Bundle myBundle = myIntent.getExtras();
+        diaryData = myBundle.getParcelable("data");
+
+        mRef = FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("diary");
+
+        //Play Music
+        Intent intent1 = new Intent(DiaryActivity.this, MusicService.class);
+        intent1.putExtra(MusicService.MESSEAGE_KEY,true);
+
+        Bundle myBundle1 = new Bundle();
+        myBundle1.putInt("emo_num", diaryData.getEmotion());
+        intent1.putExtras(myBundle);
+        startService(intent1);
+
+        int type = diaryData.getType();
+        switch (type) {
+            case 1:
+                setContentView(R.layout.activity_letter);
+
+                lDate = findViewById(R.id.Draw_Date);
+                lWeather = findViewById(R.id.Draw_Weather);
+                lTitle = findViewById(R.id.title);
+                lSubtitle = findViewById(R.id.subtitle);
+                lContent = findViewById(R.id.content);
+                saveBtn = findViewById(R.id.save);
+
+                lDate.setText(diaryData.getDate());
+                lWeather.setText(diaryData.getWeather());
+                lTitle.setText(diaryData.getTitle());
+                lSubtitle.setText(diaryData.getSubtitle());
+                lContent.setText(diaryData.getText());
+                saveBtn.setOnClickListener(DiaryActivity.this);
+
+
+                break;
+
+            case 2:
+                setContentView(R.layout.activity_draw_layout);
+
+
+                dDate = findViewById(R.id.Draw_Date);
+                dWeather = findViewById(R.id.Draw_Weather);
+                dView = findViewById(R.id.drawView);
+                dContents = findViewById(R.id.drawContents);
+
+                dDate.setText(diaryData.getDate());
+                dWeather.setText(diaryData.getWeather());
+                dContents.setText(diaryData.getText());
+
+                dView.setVisibility(View.VISIBLE);
+                dView.setImageBitmap(Utils.stringToBitmap(diaryData.getDraw()));
+                break;
+
+            case 3:
+                setContentView(R.layout.activity_picture_lay);
+
+                pDate = findViewById(R.id.Draw_Date);
+                pWeather = findViewById(R.id.Draw_Weather);
+                pView = findViewById(R.id.view);
+                pContent = findViewById(R.id.diaryPhoto);
+                pSaveBtn = findViewById(R.id.save_btn);
+                pUploadBtn = findViewById(R.id.upload_btn);
+
+                pDate.setText(diaryData.getDate());
+                pWeather.setText(diaryData.getWeather());
+
+                pView.setImageBitmap(Utils.stringToBitmap(diaryData.getPic()));
+
+                pContent.setText(diaryData.getText());
+                //pSaveBtn.setOnClickListener(DiaryActivity.this);
+                //pUploadBtn.setOnClickListener(DiaryActivity.this);
+                break;
+        }
+
+    }
+
+    public boolean onKeyDown(int keyCode,KeyEvent event){
+        switch(keyCode){
+            case android.view.KeyEvent.KEYCODE_BACK:
+                exit();
+        }
+        return false;
+    }
+
+    public void exit(){
+        //Stop Music
+        Intent intent1 = new Intent(DiaryActivity.this, MusicService.class);
+        intent1.putExtra(MusicService.MESSEAGE_KEY,false);
+        stopService(intent1);
+        finish();
+    }
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.save:
+                DiaryData newData = new DiaryData(lTitle.getText().toString(), lSubtitle.getText().toString(), lDate.getText().toString(), lWeather.getText().toString(),lContent.getText().toString(), diaryData.getEmotion(), diaryData.getType());
+                mRef.child("text").child(diaryData.getId()).setValue(newData);
+                Toast.makeText(getApplicationContext(),"수정 되었습니다.",Toast.LENGTH_LONG).show();
+                finish();
+                break;
+            case R.id.save_btn:
+                //String subtitle, int emotion, String date, String weather, String text, String pic, int type
+                DiaryData newData2 = new DiaryData(pSubtitle.getText().toString(), diaryData.getEmotion(),pDate.getText().toString(), pWeather.getText().toString(),pContent.getText().toString(), diaryData.getPic(), diaryData.getType());
+                mRef.child("picture").child(diaryData.getId()).setValue(newData2);
+                Toast.makeText(getApplicationContext(),"수정 되었습니다.",Toast.LENGTH_LONG).show();
+                finish();
+                break;
+        }
+    }
+
+
+}
---
+++ Sina_ver26/app/src/main/java/org/androidtown/sina_ver01/MusicService.java
@@ -0,0 +1,142 @@
+package org.androidtown.sina_ver01;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.view.KeyEvent;
+
+import java.io.IOException;
+
+public class MusicService extends Service {
+    public MediaPlayer mp;
+    public static String MESSEAGE_KEY;
+
+    int notificationID=1;
+    String music;
+    public MusicService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public int onStartCommand(Intent intent,int flags,int startId){
+        boolean message=intent.getExtras().getBoolean(MusicService.MESSEAGE_KEY);
+        int emotion=intent.getExtras().getInt("emo_num");
+
+        DiaryData emo=new DiaryData();
+        music=randomMusic(emotion);
+
+        mp=new MediaPlayer();
+
+        if(message){
+            try {
+                mp.setDataSource(music);
+                mp.prepare();
+                mp.start();
+                mp.setLooping(true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Intent mMainIntent=new Intent(this,SelectEmotionActivity.class);
+            mMainIntent.putExtra("notificationID",notificationID);
+            PendingIntent mPendingIntent=PendingIntent.getActivity(this,0,mMainIntent,PendingIntent.FLAG_ONE_SHOT);
+
+            NotificationCompat.Builder mBuilder=
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(android.R.drawable.btn_star)
+                            .setContentTitle("시나브로 앱 음악")
+                            .setContentIntent(mPendingIntent)
+                            .setContentText("백그라운드에서 음악이 플레이되고 있어요");
+
+            NotificationManager mNotifyMgr=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+            mNotifyMgr.notify(notificationID,mBuilder.build());
+
+        }else {
+            mp.stop();
+            mp.release();
+        }
+        return START_NOT_STICKY;
+    }
+
+    public void onDestroy(){
+        super.onDestroy();
+        mp.stop();
+        mp.release();
+    }
+    public String randomMusic(int emotion)
+    {
+        String emoString="";
+        int random=(int)(Math.random()*4)+1;
+
+        switch(emotion){
+            case 0:
+                emoString=happyMusic(random);
+                break;
+            case 1:
+                emoString=sadMusic(random);
+                break;
+            case 2:
+                emoString=angryMusic(random);
+                break;
+            default:
+                break;
+        }
+        return emoString;
+    }
+
+
+    public String happyMusic(int random){
+        String emoString="";
+
+        if(random==1) {
+            emoString="https://firebasestorage.googleapis.com/v0/b/sinabro-1526195392107.appspot.com/o/music%2Fhappy%2FBed_and_Breakfast.mp3?alt=media&token=c269cdc2-d26d-4e90-ac11-7fdf90f13204";
+        } else if(random==2) {
+            emoString = "https://firebasestorage.googleapis.com/v0/b/sinabro-1526195392107.appspot.com/o/music%2Fhappy%2FBongo_Madness.mp3?alt=media&token=e64e7ba4-6ce0-48ae-918d-fa0b004c5c23";
+        } else if(random==3){
+            emoString="https://firebasestorage.googleapis.com/v0/b/sinabro-1526195392107.appspot.com/o/music%2Fhappy%2FFond_Memories.mp3?alt=media&token=a5f07d41-7514-4558-9beb-8947025d7e48";
+        }else if(random==4){
+            emoString="https://firebasestorage.googleapis.com/v0/b/sinabro-1526195392107.appspot.com/o/music%2Fhappy%2FSpecial_Ed.mp3?alt=media&token=dfb046ee-4a1d-4024-aa94-96f4235532ce";
+        }
+        return emoString;
+    }
+
+    public String sadMusic(int random){
+        String emoString="";
+
+        if(random==1)
+            emoString="https://firebasestorage.googleapis.com/v0/b/sinabro-1526195392107.appspot.com/o/music%2Fsad%2FCampfire_Song.mp3?alt=media&token=58a929c1-46dc-4812-9c97-a405b090747a";
+        else if(random==2)
+            emoString="https://firebasestorage.googleapis.com/v0/b/sinabro-1526195392107.appspot.com/o/music%2Fsad%2FClover_3.mp3?alt=media&token=d37a40bc-9bb8-461a-ab8b-6f0346474397";
+        else if(random==3)
+            emoString="https://firebasestorage.googleapis.com/v0/b/sinabro-1526195392107.appspot.com/o/music%2Fsad%2FNew_Year_s_Anthem.mp3?alt=media&token=c9304667-b11f-44c2-94d5-819eefba3505";
+        else if(random==4)
+            emoString="https://firebasestorage.googleapis.com/v0/b/sinabro-1526195392107.appspot.com/o/music%2Fsad%2FTake_Me_to_the_Depths.mp3?alt=media&token=71f2b233-97f3-4185-96c0-cc0b4549ea22";
+
+        return emoString;
+    }
+
+    public String angryMusic(int random){
+        String emoString="";
+
+        if(random==1)
+            emoString="https://firebasestorage.googleapis.com/v0/b/sinabro-1526195392107.appspot.com/o/music%2Fangry%2FDarkness_of_My_Sun.mp3?alt=media&token=a3435ff4-47d2-492c-b529-1e2f6dd9afe0";
+        else if(random==2)
+            emoString="https://firebasestorage.googleapis.com/v0/b/sinabro-1526195392107.appspot.com/o/music%2Fangry%2FGhost_Chase_Thriller.mp3?alt=media&token=3bbae31a-38e7-4c97-ae67-1e1d66816554";
+        else if(random==3)
+            emoString="https://firebasestorage.googleapis.com/v0/b/sinabro-1526195392107.appspot.com/o/music%2Fangry%2FSpring.mp3?alt=media&token=f39a778d-c3a3-4c9c-bcc2-cfb77da9a942";
+        else if(random==4)
+            emoString="https://firebasestorage.googleapis.com/v0/b/sinabro-1526195392107.appspot.com/o/music%2Fangry%2FYou_re_Not_Wrong.mp3?alt=media&token=c27933a8-9609-4dd3-a3b9-48047ffcfffa";
+        return emoString;
+    }
+
+}
