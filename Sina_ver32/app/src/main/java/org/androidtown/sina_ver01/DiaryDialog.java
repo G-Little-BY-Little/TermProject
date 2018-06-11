2b0451aade2b87ef9d4d5dbd846738368a19d225
genie97 <qndnd1@naver.com>
2018-06-11 10:37:50 +0900

    app_ver2

---
+++ Sina_ver32/app/src/main/java/org/androidtown/sina_ver01/DiaryDialog.java
@@ -0,0 +1,36 @@
+package org.androidtown.sina_ver01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class DiaryDialog extends AppCompatActivity {
+
+    Button read,write;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_dialog);
+        read = (Button)findViewById(R.id.readBtn);
+        write = (Button)findViewById(R.id.writeBtn);
+
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SelectEmotionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ShowActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
